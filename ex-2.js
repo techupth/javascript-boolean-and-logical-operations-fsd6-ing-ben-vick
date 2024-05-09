a72ee4d29e3bd3730ec09669e380f5a787369308
Ben Wattanasup <benwata777@gmail.com>
2024-05-09 15:57:29 +0700

    java

--- ex-2.js
+++ ex-2.js
@@ -1,12 +1,21 @@
 // Exercise #2: Promotion Conditions
 
 // Start coding here
-let lastMonthPaidMoreThan4000;
-let isWeekday;
-let hasBoughtProductFromITCategory;
-let hasAttendedDiscountEvent;
-let isPlatinum;
 
-let hasPromotion;
+let paid = 4001;
+let member = "gold";
+
+let lastMonthPaidMoreThan4000 = paid > 4000;
+let isWeekday = true;
+let hasBoughtProductFromITCategory = true;
+let hasAttendedDiscountEvent = true;
+let isPlatinum = member == "platinum";
+
+let hasPromotion =
+  (lastMonthPaidMoreThan4000 &&
+    isWeekday &&
+    !hasBoughtProductFromITCategory &&
+    !hasAttendedDiscountEvent) ||
+  isPlatinum;
 
 console.log(hasPromotion);
